7907e5a4b619743c313123a15277a98774133e0d
Komiljon <turayev.komiljon1997@gmail.com>
2021-06-03 11:18:10 +0500

    Initial commit

---
+++ app/src/main/java/uz/uzsoft/qqbtrans/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.uzsoft.qqbtrans
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
