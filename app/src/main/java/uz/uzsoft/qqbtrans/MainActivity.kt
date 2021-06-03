52ff2bdbd9a7e75489a28bc0c3f0115f5e398e17
Komiljon <turayev.komiljon1997@gmail.com>
2021-06-03 11:33:54 +0500

    First Toast

--- app/src/main/java/uz/uzsoft/qqbtrans/MainActivity.kt
+++ app/src/main/java/uz/uzsoft/qqbtrans/MainActivity.kt
@@ -2,10 +2,13 @@ package uz.uzsoft.qqbtrans
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "First Toast", Toast.LENGTH_SHORT).show()
     }
 }
