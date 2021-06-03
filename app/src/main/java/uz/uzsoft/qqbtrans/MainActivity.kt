1f04db3567f173c56e06124a0dd64b205f7f09ca
Komiljon <turayev.komiljon1997@gmail.com>
2021-06-03 23:42:56 +0500

    added bottom navigations

--- app/src/main/java/uz/uzsoft/qqbtrans/MainActivity.kt
+++ app/src/main/java/uz/uzsoft/qqbtrans/MainActivity.kt
@@ -3,6 +3,11 @@ package uz.uzsoft.qqbtrans
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import uz.uzsoft.qqbtrans.R
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +16,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "First Toast", Toast.LENGTH_SHORT).show()
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.nav_main, R.id.nav_trans, R.id.nav_pay,R.id.nav_more,R.id.nav_monitoring))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
+
 }
